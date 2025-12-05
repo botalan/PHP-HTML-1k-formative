e8c60fa5f57365eab6aedd99a7791aefbee0eb3b
unknown <bruno@example.com>
2025-12-05 15:58:42 +0200

    About.php

--- about.php
+++ about.php
@@ -1,1 +1,20 @@
-<!-- your code goes here -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+    <a href="/index.html">Home</a>
+    <a href="/contact.php">Contact</a>
+    </nav>
+    <?php
+    $indexArray = ["Lorem ipsum dolor sit amet.","Lorem ipsum dolor sit amet, consectetur adipisicing.","Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, iusto."];
+    foreach($indexArray as $arr){
+        echo "<p>".$arr."</p>";
+    }
+    ?>
+</body>
+</html>
